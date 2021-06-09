55ba99171cc91b2894d8a5b3768f6be1f6224e8d
Nikita Stetskiy <nikin929@gmail.com>
2021-06-09 10:21:37 +0200

    Corrijo entrega de IA

--- 2º/IA/prácticas/P3/Desconecta4Boom_windowsV2/player.cpp
+++ 2º/IA/prácticas/P3/Desconecta4Boom_windowsV2/player.cpp
@@ -53,14 +53,196 @@ double ValoracionTest(const Environment &estado, int jugador){
 
 // ------------------- Los tres metodos anteriores no se pueden modificar
 
+double Player::Heuristica(const Environment &estado ,int fil, int col){
 
+     // 4 FILA
+   if ((col<4 && estado.See_Casilla(fil,col)) && (estado.See_Casilla(fil,col+2)) == (estado.See_Casilla(fil,col+3))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil,col+1))
+   && (estado.See_Casilla(fil,col+1)) == (estado.See_Casilla(fil,col+2)))){
 
-// Funcion heuristica (ESTA ES LA QUE TENEIS QUE MODIFICAR)
-double Valoracion(const Environment &estado, int jugador){
+      return((estado.See_Casilla(fil,col) == jugador_) ? -999999.0 : 999999.0);
+   }
+
+   // 4 COLUMNA
+   if ((fil<4) && (estado.See_Casilla(fil+2,col)) == (estado.See_Casilla(fil+3,col))
+   && (estado.See_Casilla(fil+1,col)) == (estado.See_Casilla(fil+2,col))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -999999.0 : 999999.0);
+   }
+
+   // 4 DIAGONAL
+   if ((fil<4 && col<4) && (estado.See_Casilla(fil+2,col+2)) == (estado.See_Casilla(fil+3,col+3))
+   && (estado.See_Casilla(fil+1,col+1)) == (estado.See_Casilla(fil+2,col+2))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -999999.0 : 999999.0);
+   }
+
+   if ((fil>2 && col<4) && (estado.See_Casilla(fil-2,col+2)) == (estado.See_Casilla(fil-3,col+3))
+   && (estado.See_Casilla(fil-1,col+1)) == (estado.See_Casilla(fil-2,col+2))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil-1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -999999.0 : 999999.0);
+   }
+
+   // 3 FILA
+   if ((col<5 && estado.See_Casilla(fil,col) ) && (
+   (estado.See_Casilla(fil,col+1)) == (estado.See_Casilla(fil,col+2)) &&
+   (estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -99.0 : 99.0);
+   }
+
+   // 3 COLUMNA
+   if ((fil<5) && (estado.See_Casilla(fil+1,col)) == (estado.See_Casilla(fil+2,col))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -99.0 : 99.0);
+   }
+
+   // 3 DIAGONAL
+   if ((fil>1 && col<5) && (estado.See_Casilla(fil-1,col+1)) == (estado.See_Casilla(fil-2,col+2))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil-1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -99.0 : 99.0);
+   }
+
+   if ((fil<5 && col<5) && (estado.See_Casilla(fil+1,col+1)) == (estado.See_Casilla(fil+2,col+2))
+   && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -99.0 : 99.0);
+   }
+
+   // 2 FILA
+   if ((col<6 && estado.See_Casilla(fil,col)) &&
+   ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -9.0 : 9.0);
+   }
+
+   // 2 COLUMNA
+   if ((fil<6) && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -9.0 : 9.0);
+   }
+
+   // 2 DIAGONAL
+   if ((fil>0 and col<6) && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil-1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -9.0 : 9.0);
+   }
+
+   if ((fil<6 and col<6) && ((estado.See_Casilla(fil,col)) == (estado.See_Casilla(fil+1,col+1)))){
+
+      return((estado.See_Casilla(fil,col) == jugador_) ? -9.0 : 9.0);
+   }
+
+   return 0.0;
+
+}
+
+double Player::Poda_AlfaBeta(Environment entorno , int prof ,int &acc, double alpha , double beta ){
+
+   if(prof == 0 || entorno.JuegoTerminado()) {
+
+		if(entorno.JuegoTerminado()){
+			return ValoracionTest(entorno,jugador_);
+		}
+		return ValoracionTest2(entorno,jugador_);
+
+	}
+
+   Environment e_n ;
+	double bestNodo;
+	int aux = -1;
+	e_n = entorno.GenerateNextMove(aux);
+
+   if(entorno.JugadorActivo() != jugador_ ){
+
+		bestNodo = beta;
+
+		while(aux<8){
+			int otra = aux;
+
+			double nodoHijo = Poda_AlfaBeta(e_n , prof -1 ,otra, alpha , bestNodo);
+
+			if(nodoHijo < bestNodo && aux != -1 && prof == 8){
+				acc = aux;
+			}
+
+			bestNodo = min( bestNodo , nodoHijo);
+            if (bestNodo <= alpha) {
+                break;
+            }
+			e_n = entorno.GenerateNextMove(aux);
+
+		}
+
+	}else{
+
+		bestNodo = alpha;
+
+		while(aux<8){
+			int otra = aux;
+
+			double nodoHijo = Poda_AlfaBeta(e_n , prof -1 ,otra, bestNodo , beta);
+
+			if(nodoHijo > bestNodo && aux != -1 && prof == 8){
+				acc = aux;
+			}
+
+			bestNodo = max( bestNodo , nodoHijo);
+
+			if (beta <= bestNodo) {
+                break;
+            }
+			e_n = entorno.GenerateNextMove(aux);
+		}
+	}
+   return bestNodo;
 }
 
 
 
+double Player::ValoracionTest2(const Environment &estado, int jugador){
+
+int ganador = estado.RevisarTablero();
+
+    if (ganador==jugador)
+       return 99999999.0; // Gana el jugador que pide la valoracion
+    else if (ganador!=0)
+            return -99999999.0; // Pierde el jugador que pide la valoracion
+    else if (estado.Get_Casillas_Libres()==0)
+            return 0;  // Hay un empate global y se ha rellenado completamente el tablero
+    else{
+
+		double suma = Heuristica(estado,0,0);
+		int j = 0;
+
+		for (int i=0; i<7; i++)
+	      for (int j=0; j<7; j++){
+
+	      	if(jugador != jugador_){
+                if (estado.See_Casilla(i,j)!=0 && suma < Heuristica(estado,i,j) )
+	             suma = Heuristica(estado,i,j);
+
+	      	}else{
+                if (estado.See_Casilla(i,j)!=0 && suma > Heuristica(estado,i,j) )
+                 suma = Heuristica(estado,i,j);
+	      	}
+	      }
+
+    	return suma;
+    }
+
+
+}
+
+// Funcion heuristica (ESTA ES LA QUE TENEIS QUE MODIFICAR)
+double Valoracion(const Environment &estado, int jugador){
+   return Puntuacion(jugador, estado);
+}
 
 
 // Esta funcion no se puede usar en la version entregable
@@ -76,31 +258,17 @@ void JuegoAleatorio(bool aplicables[], int opciones[], int &j){
     }
 }
 
-
-
-
-
-
 // Invoca el siguiente movimiento del jugador
 Environment::ActionType Player::Think(){
     const int PROFUNDIDAD_MINIMAX = 6;  // Umbral maximo de profundidad para el metodo MiniMax
     const int PROFUNDIDAD_ALFABETA = 8; // Umbral maximo de profundidad para la poda Alfa_Beta
 
-    Environment::ActionType accion; // acción que se va a devolver
-    bool aplicables[8]; // Vector bool usado para obtener las acciones que son aplicables en el estado actual. La interpretacion es
-                        // aplicables[0]==true si PUT1 es aplicable
-                        // aplicables[1]==true si PUT2 es aplicable
-                        // aplicables[2]==true si PUT3 es aplicable
-                        // aplicables[3]==true si PUT4 es aplicable
-                        // aplicables[4]==true si PUT5 es aplicable
-                        // aplicables[5]==true si PUT6 es aplicable
-                        // aplicables[6]==true si PUT7 es aplicable
-                        // aplicables[7]==true si BOOM es aplicable
-
+    Environment::ActionType accion; // acciï¿½n que se va a devolver
+    bool aplicables[8];
 
 
     double valor; // Almacena el valor con el que se etiqueta el estado tras el proceso de busqueda.
-    double alpha, beta; // Cotas de la poda AlfaBeta
+    double alpha = -10000000000000.0, beta= 10000000000000.0; // Cotas de la poda AlfaBeta
 
     int n_act; //Acciones posibles en el estado actual
 
@@ -108,8 +276,6 @@ Environment::ActionType Player::Think(){
     n_act = actual_.possible_actions(aplicables); // Obtengo las acciones aplicables al estado actual en "aplicables"
     int opciones[10];
 
-    // Muestra por la consola las acciones aplicable para el jugador activo
-    //actual_.PintaTablero();
     cout << " Acciones aplicables ";
     (jugador_==1) ? cout << "Verde: " : cout << "Azul: ";
     for (int t=0; t<8; t++)
@@ -117,40 +283,23 @@ Environment::ActionType Player::Think(){
          cout << " " << actual_.ActionStr( static_cast< Environment::ActionType > (t)  );
     cout << endl;
 
+    int accionAuxiliar = -1;
+    valor = Poda_AlfaBeta(actual_, 8, accionAuxiliar, menosinf, masinf);
 
-    //--------------------- COMENTAR Desde aqui
-    cout << "\n\t";
-    int n_opciones=0;
-    JuegoAleatorio(aplicables, opciones, n_opciones);
+   cout << accionAuxiliar;
 
-    if (n_act==0){
-      (jugador_==1) ? cout << "Verde: " : cout << "Azul: ";
-      cout << " No puede realizar ninguna accion!!!\n";
-      //accion = Environment::actIDLE;
-    }
-    else if (n_act==1){
-           (jugador_==1) ? cout << "Verde: " : cout << "Azul: ";
-            cout << " Solo se puede realizar la accion "
-                 << actual_.ActionStr( static_cast< Environment::ActionType > (opciones[0])  ) << endl;
-            accion = static_cast< Environment::ActionType > (opciones[0]);
+	switch (accionAuxiliar)
+	{
+	case 0: return Environment::PUT1;
+	case 1: return Environment::PUT2;
+	case 2: return Environment::PUT3;
+	case 3: return Environment::PUT4;
+	case 4: return Environment::PUT5;
+	case 5: return Environment::PUT6;
+	case 6: return Environment::PUT7;
+	case 7: return Environment::BOOM;
 
-         }
-         else { // Hay que elegir entre varias posibles acciones
-            int aleatorio = rand()%n_opciones;
-            cout << " -> " << actual_.ActionStr( static_cast< Environment::ActionType > (opciones[aleatorio])  ) << endl;
-            accion = static_cast< Environment::ActionType > (opciones[aleatorio]);
-         }
-
-    //--------------------- COMENTAR Hasta aqui
-
-
-    //--------------------- AQUI EMPIEZA LA PARTE A REALIZAR POR EL ALUMNO ------------------------------------------------
-
-
-    // Opcion: Poda AlfaBeta
-    // NOTA: La parametrizacion es solo orientativa
-    // valor = Poda_AlfaBeta(actual_, jugador_, 0, PROFUNDIDAD_ALFABETA, accion, alpha, beta);
-    //cout << "Valor MiniMax: " << valor << "  Accion: " << actual_.ActionStr(accion) << endl;
+	}
 
     return accion;
 }
--- 2º/IA/prácticas/P3/Desconecta4Boom_windowsV2/player.h
+++ 2º/IA/prácticas/P3/Desconecta4Boom_windowsV2/player.h
@@ -5,9 +5,19 @@
 
 class Player{
     public:
-      Player(int jug);
-      Environment::ActionType Think();
-      void Perceive(const Environment &env);
+      	Player(int jug);
+     	Environment::ActionType Think();
+     	void Perceive(const Environment &env);
+
+
+	  	double Valoracion(const Environment &estado, int jugador);
+
+	  	//Metodos propios de la practica ->
+
+	 	double Poda_AlfaBeta(Environment entorno , int profundidad ,int &accion, double alpha , double beta );
+		double ValoracionTest2(const Environment &estado, int jugador);
+		double Heuristica(const Environment &estado ,int fila, int columna);
+
     private:
       int jugador_;
       Environment actual_;
